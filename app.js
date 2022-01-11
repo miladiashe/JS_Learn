f6a63847866bf17ffcad8ecc81c7e4d41977aeab
miladiashe <dkdud0122@naver.com>
2022-01-12 00:39:35 +0900

    공부중...
    const, let

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const a = 5;
 const b = 2;
+let myName = "kala";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+
+console.log("안녕 "+myName);
+
+myName = "Miladiashe";
+
+console.log("안녕안녕 "+myName);
