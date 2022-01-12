add997a42777a9db57069de4ad9e38ee2b5101e4
miladiashe <dkdud0122@naver.com>
2022-01-12 21:46:43 +0900

    프롬프트는 보통 안쓴다고 한다.
    그렇다.

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-const calculator = {
-    add : function(a, b){
-        return a+b;
-    },
+const age = prompt("나이.");
 
-    minus : function(a, b){
-        return a-b;
-    },
-
-    divide : function(a, b){
-        return a/b;
-    },
-
-    powerOf : function(a, b){
-        return a**b;
-    },
-
-    times : function(a, b){
-        return a*b
-    }
-
-}
-
-const plusResult = calculator.add(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.powerOf(10, minusResult);
-const divResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.powerOf(divResult, minusResult);
+console.log(age)
