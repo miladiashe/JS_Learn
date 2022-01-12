24a3dd331e3d7fc3834437a9e7b2f7475cb983e1
miladiashe <dkdud0122@naver.com>
2022-01-12 20:40:30 +0900

    함수는 이렇게 쓰는게 맞다.
    공부공부

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const calculator = {
     add : function(a, b){
-        console.log(a+b);
+        return a+b;
     },
 
     minus : function(a, b){
-        console.log(a-b);
+        return a-b;
     },
 
     divide : function(a, b){
-        console.log(a/b);
+        return a/b;
     },
 
     powerOf : function(a, b){
-        console.log(a**b);
+        return a**b;
     },
 
 }
 
+calculator.add(97, 22);
+calculator.divide(21, 3);
+calculator.minus(50, 48);
+calculator.powerOf(2, 2);
