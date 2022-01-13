b66afea5114a7a84d5e15fc89ccbc70dda71c1cf
miladiashe <dkdud0122@naver.com>
2022-01-13 19:00:01 +0900

    자바스크립트와 HTML을 풀칠하기

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-const age = parseInt(prompt("나이."));
+const title = document.getElementById("title");
 
-
-
-if(isNaN(age) || age<0){
-    console.log("양수를 적어라")
-}else if(age < 19){
-    console.log("집에 가서 자라")
-} else if(age >= 18 && age <= 50){
-    console.log("술 많이 먹자")
-} else if( age>50 && age<=80){
-    console.log("아조씨.....")
-} else{
-    console.log("건강을 생각하셔야죠 어르신")
-}
+console.dir(title)
