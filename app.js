70919b1579e9673cb11c8906b3b1a980a547677d
miladiashe <dkdud0122@naver.com>
2022-01-14 00:31:28 +0900

    클릭하면 색 바꾸기+if문

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
 const title = document.querySelector(".title h1");
 
 function handleTitleClick() {
-    title.style.color = "red"
-}
+    const currentColor = title.style.color
+    let newColor
+    if (currentColor === "blue") {
+        newColor = "pink"
+    }else{
+        newColor = "blue"
+    }
+    title.style.color = newColor
 
-function handleMouseEnter() {
-    title.style.color = "Violet"
 }
-
-function handleMouseLeave() {
-    title.style.color = "Purple"
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "Yellow"
-    
-}
-
-function handleOffline() {
-    alert("살려주세오 와이파이가 없어오")
-}
-
-function handleOnline() {
-    alert("살았어오 와이파이가 있어오")
-}
-
 
 title.onclick = handleTitleClick
-title.addEventListener("mouseenter", handleMouseEnter)
-title.addEventListener("mouseleave", handleMouseLeave)
-
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("offline", handleOffline)
-window.addEventListener("online", handleOnline)
