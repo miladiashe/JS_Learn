7ee4cebcd40396bc835bb3194eb0029dd16a4ec3
miladiashe <dkdud0122@naver.com>
2022-01-14 01:30:35 +0900

    쉬운방법
    토글

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const title = document.querySelector(".title h1");
 
 function handleTitleClick() {
     const clickedClass = "clicked"
-    if (title.classList.contains(clickedClass)){
-        title.classList.remove(clickedClass)
-    }else{
-        title.classList.add(clickedClass)
-    }
+    title.classList.toggle(clickedClass)
 }
 
 title.onclick = handleTitleClick
