16445b8a031841060cbc2600677dc0a92225316e
miladiashe <dkdud0122@naver.com>
2022-01-13 16:45:39 +0900

    else if

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const age = parseInt(prompt("나이."));
 
 if(isNaN(age)){
     console.log("숫자를 적어라")
-}else{
-    console.log("고맙다")
+}else if(age < 19){
+    console.log("집에 가서 자라")
+} else{
+    console.log("소주 만병만 주소")
 }
