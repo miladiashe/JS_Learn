c5db164b34f31f517e4b1305ccbfeb9897cd00cc
miladiashe <dkdud0122@naver.com>
2022-01-14 00:06:13 +0900

    이런것도 된다는게 신기하다
    신기

--- app.js
+++ app.js
@@ -12,7 +12,24 @@ function handleMouseLeave() {
     title.style.color = "Purple"
 }
 
+function handleWindowResize() {
+    document.body.style.backgroundColor = "Yellow"
+    
+}
+
+function handleOffline() {
+    alert("살려주세오 와이파이가 없어오")
+}
+
+function handleOnline() {
+    alert("살았어오 와이파이가 있어오")
+}
+
+
 title.onclick = handleTitleClick
 title.addEventListener("mouseenter", handleMouseEnter)
 title.addEventListener("mouseleave", handleMouseLeave)
 
+window.addEventListener("resize", handleWindowResize)
+window.addEventListener("offline", handleOffline)
+window.addEventListener("online", handleOnline)
