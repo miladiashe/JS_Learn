112f8867636685d29c6eda36ec871c8136511cf9
miladiashe <dkdud0122@naver.com>
2022-01-13 17:07:08 +0900

    AND 연산자

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ if(isNaN(age)){
     console.log("숫자를 적어라")
 }else if(age < 19){
     console.log("집에 가서 자라")
+} else if(age >= 18 && age <= 50){
+    console.log("술 많이 먹자")
 } else{
-    console.log("소주 만병만 주소")
+    console.log("건강을 생각하셔야죠 어르신")
 }
