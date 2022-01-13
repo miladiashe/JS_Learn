877590358d9d9e136cf0f50818778cfe53b66e1a
miladiashe <dkdud0122@naver.com>
2022-01-14 01:43:25 +0900

    제로부터 시작하는
    코딩생활

--- app.js
+++
@@ -1,8 +0,0 @@
-const title = document.querySelector(".title h1");
-
-function handleTitleClick() {
-    const clickedClass = "clicked"
-    title.classList.toggle(clickedClass)
-}
-
-title.onclick = handleTitleClick
