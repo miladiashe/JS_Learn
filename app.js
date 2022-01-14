f07f162e205387db688302e71dd71766c166b96c
miladiashe <dkdud0122@naver.com>
2022-01-14 20:14:00 +0900

    뭔가...뭔가 되고있음

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
-    console.log(userName)
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
