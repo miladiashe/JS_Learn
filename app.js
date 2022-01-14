dccb3107602b83eadd32c67b9979a9a0e29db340
miladiashe <dkdud0122@naver.com>
2022-01-14 11:53:08 +0900

    인풋 내용 찾기

---
+++ app.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function handleLoginBtnClick() {
+    console.log(loginInput.value)
+}
+
+loginButton.addEventListener("click", handleLoginBtnClick)
