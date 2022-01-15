8da2a7f7a17c3af2a8f76ee321e9cb11438d211e
miladiashe <dkdud0122@naver.com>
2022-01-15 16:44:14 +0900

    함수화
    나는 잘하고 있다

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
-    localStorage.setItem("username", userName);
+    localStorage.setItem(USERNAME_KEY, userName);
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${userName}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(savedUsername);
+}
