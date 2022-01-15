c6c828c742d995975d7906e4e0e958486ffb81fa
miladiashe <dkdud0122@naver.com>
2022-01-15 18:25:19 +0900

    시계 만들었다!
    와!

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,8 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
     const date = new Date();
-    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
 
+getClock();
 setInterval(getClock, 1000);
