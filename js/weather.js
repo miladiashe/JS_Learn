bb84424edfe208c0f56ede3a20f00fe0b5551986
miladiashe <dkdud0122@naver.com>
2022-01-16 16:28:06 +0900

    완성. css중

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "331d993570eb0dd2f86a02d83147e285"
+
+function onGeoOK(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data =>{
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const cityContainer = document.querySelector("#weather span:last-child");
+        cityContainer.innerText = data.name;
+        weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("몰?루");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
