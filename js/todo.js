99f61089b1d0c16f790f3dd52a9ed5ef6562a1b3
miladiashe <dkdud0122@naver.com>
2022-01-16 13:09:27 +0900

    좀더 개발하였다

--- js/todo.js
+++ js/todo.js
@@ -1,21 +1,54 @@
 const toDoForm = document.getElementById("todo-form");
-const toDOInput = toDoForm.querySelector("input");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 function paintTodo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
+    span.innerText = newToDo.text;
+    const btn = document.createElement("button");
+    btn.innerText = '❌'
+    btn.addEventListener("click",deleteTodo);
     li.appendChild(span);
-    span.innerText = newToDo;
+    li.appendChild(btn);
     toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
     event.preventDefault();
-    const newToDo = toDOInput.value;
-    toDOInput.value="";
-    paintTodo(newToDo);
-
+    const newToDo = toDoInput.value;
+    toDoInput.value="";
+    const newToDoObj = {
+        text: newToDo,
+        id: Date.now(),
+    };
+    toDos.push(newToDoObj);
+    paintTodo(newToDoObj);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintTodo);
+}
+
+
